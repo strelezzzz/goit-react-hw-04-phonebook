6e356fd7964d01043cd5ada7f8a407f326324b27
Taras Bondar <strelezzzz@gmail.com>
2022-07-21 18:07:15 +0300

    step 4 ok

--- src/components/App.js
+++ src/components/App.js
@@ -14,8 +14,6 @@ export class App extends Component {
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
-    name: '',
-    number: '',
   };
   //додає контакт у стейт
   addContacts = (name, number) => {
@@ -31,24 +29,13 @@ export class App extends Component {
       contacts: [contact, ...contacts],
     }));
   };
+
   //відслідковує зміну value(відповідного елемента за  name="?" ) , та записує в стейт;
   handleChange = e => {
     // console.log('event: ', e.currentTarget.value);
     this.setState({ [e.target.name]: e.target.value });
-    console.log('event: ', e.target.name, 'VALUE:', e.target.value);
   };
-  //скидає значення форми до початкового значення;
-  reset = () => {
-    this.setState({ name: '', number: '' });
-  };
-  //викликається при сабміті;
-  handleSubmit = e => {
-    e.preventDefault();
-    const { name, number } = this.state;
-    this.addContacts(name, number);
-    this.reset();
-  };
-  //повертає новий масив контактів, які проходять фільтр
+  //повертає новий масив obj - контактів, які проходять фільтр
   getVisibleContacts = () => {
     const { filter, contacts } = this.state;
     const normalizedFilter = filter.toLocaleLowerCase();
@@ -58,18 +45,13 @@ export class App extends Component {
   };
 
   render() {
-    const { filter, name, number } = this.state;
+    const { filter } = this.state;
     const visibleContacts = this.getVisibleContacts();
-    console.log(visibleContacts);
+
     return (
       <div>
         <h1>Phonebook</h1>
-        <ContactForm
-          onSubmit={this.handleSubmit}
-          onChange={this.handleChange}
-          name={name}
-          number={number}
-        />
+        <ContactForm onSubmit={this.addContacts} />
         <h2>Contacts</h2>
         <Filter onChange={this.handleChange} value={filter} />
         <ContactList contacts={visibleContacts} />
--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,35 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const ContactForm = ({ onSubmit, onChange, name, number }) => (
-  <form name="ContactForm" onSubmit={onSubmit}>
-    <label>
-      Name
-      <input
-        onChange={onChange}
-        value={name}
-        type="text"
-        name="name"
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        required
-      />
-    </label>
-    <label>
-      Number
-      <input
-        onChange={onSubmit}
-        value={number}
-        type="tel"
-        name="number"
-        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-        required
-      />
-    </label>
-    <button type="submit" className="">
-      Add contact
-    </button>
-  </form>
-);
+class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+  //скидає значення форми до початкового значення;
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+  //сабміт форми та передача пропів;
+  handleSubmit = e => {
+    e.preventDefault();
+    const { name, number } = this.state;
+    // Проп, який передається формі для виклику під час сабміту;
+    this.props.onSubmit(name, number);
+    this.reset();
+  };
+
+  //відслідковує зміну value(відповідного елемента за  name="?" ) , та записує в стейт;
+  handleChange = e => {
+    // console.log('event: ', e.currentTarget.value);
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    const { name, number } = this.state;
+
+    return (
+      <form name="ContactForm" onSubmit={this.handleSubmit}>
+        <label>
+          Name
+          <input
+            onChange={this.handleChange}
+            value={name}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <label>
+          Number
+          <input
+            onChange={this.handleChange}
+            value={number}
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+        <button type="submit" className="">
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
+// ({ onSubmit, onChange, name, number }) =>
 
 export default ContactForm;
